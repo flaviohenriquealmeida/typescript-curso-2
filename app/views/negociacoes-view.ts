8a868dec3ce18cc4490ed108c87da692278944c1
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-03-22 13:18:53 +0000

    video1.3

---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,17 @@
+export class NegociacoesView {
+    template(): string {
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+        `;
+    }
+}
