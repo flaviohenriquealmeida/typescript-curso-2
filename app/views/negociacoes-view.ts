57a3408b1a09fec2d66b8cc8efbea6e78b999352
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-03-22 16:25:46 +0000

    video3.2

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -18,8 +18,7 @@ export class NegociacoesView extends View<Negociacoes> {
                 ${model.lista().map(negociacao => {
                     return `
                         <tr>
-                            <td>${new Intl.DateTimeFormat()
-                                .format(negociacao.data)}
+                            <td>${this.formatar(negociacao.data)}
                             </td>
                             <td>
                                 ${negociacao.quantidade}
@@ -33,5 +32,10 @@ export class NegociacoesView extends View<Negociacoes> {
             </tbody>
         </table>
         `;
+    }
+
+    private formatar(data: Date): string {
+        return new Intl.DateTimeFormat()
+            .format(data);
     }
 }
--- dist/js/views/negociacoes-view.js
+++ dist/js/views/negociacoes-view.js
@@ -14,8 +14,7 @@ export class NegociacoesView extends View {
                 ${model.lista().map(negociacao => {
             return `
                         <tr>
-                            <td>${new Intl.DateTimeFormat()
-                .format(negociacao.data)}
+                            <td>${this.formatar(negociacao.data)}
                             </td>
                             <td>
                                 ${negociacao.quantidade}
@@ -29,5 +28,9 @@ export class NegociacoesView extends View {
             </tbody>
         </table>
         `;
+    }
+    formatar(data) {
+        return new Intl.DateTimeFormat()
+            .format(data);
     }
 }
