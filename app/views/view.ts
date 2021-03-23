30b4b7cd0d845e19cbc50a784c1b67a602d6ed36
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-03-23 16:44:46 +0000

    video5.5

--- app/views/view.ts
+++ app/views/view.ts
@@ -4,7 +4,12 @@ export abstract class View<T> {
     private escapar = false;
 
     constructor(seletor: string, escapar?: boolean) {
-        this.elemento = document.querySelector(seletor);
+        const elemento = document.querySelector(seletor);
+        if (elemento) {
+            this.elemento = elemento as HTMLElement;
+        } else {
+            throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
+        }
         if (escapar) {
             this.escapar = escapar;
         }
